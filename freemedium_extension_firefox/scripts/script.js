479fc453ce73d6b802f8108b74f8aaa25f91a1b4
saqib <saqib.aminul29@gmail.com>
2024-10-08 23:29:31 +0600

    add free medium extension for Firefox

---
+++ freemedium_extension_firefox/scripts/script.js
@@ -0,0 +1,20 @@
+browser.action.onClicked.addListener((tab) => {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        let currentTab = tabs[0];
+        let url = currentTab.url;
+
+        if(url.includes("medium.com")) {
+            let newUrl = `https://freedium.cfd/${url}`;
+
+            browser.tabs.create({ url: newUrl, index: currentTab.index + 1 });
+        } else {
+            browser.notifications.create({
+                type: 'basic',
+                iconUrl: '../images/icon-64.png',
+                title: 'Wrong site',
+                message: 'This extension only works on medium.com',
+                priority: 2
+            });
+        }
+    });
+});
