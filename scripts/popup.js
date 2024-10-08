845d39ef0a1f1c9221c62d7c04aefdd234f50d4e
saqib <saqib.aminul29@gmail.com>
2024-10-08 19:47:59 +0600

    update the extention

--- scripts/popup.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "URL_MISMATCH") {
-      document.querySelector('.message').textContent = "This extension only works on medium.com";
-    }
-  });
-  
